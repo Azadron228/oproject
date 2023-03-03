7ab405454ec50b83c2f672a1e99533a44059570c
Azardo5300 <ibrayv2005@gmail.com>
2023-03-03 15:09:44 +0600

    commit1

---
+++ src/main/java/com/azardo5300/oproject/OprojectApplication.java
@@ -0,0 +1,13 @@
+package com.azardo5300.oproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azardo5300/oproject/OprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azardo5300.oproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
